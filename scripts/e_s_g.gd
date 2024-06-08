600c3a26c55d72538539ce47b5fcc53d0df51243
Luiz Rosa <luiz.alberto.laus.rosa@gmail.com>
2024-06-07 21:32:17 -0300

    Feature: ESG class encapsulation

---
+++ scripts/e_s_g.gd
@@ -0,0 +1,120 @@
+class_name ESG extends Object
+
+
+var esg: Array
+
+enum ESG_GROUPS {ENVIROMENT, SOCIAL, GOVERNANCE}
+
+
+static var yes_group = range(0, 24)
+static var yes_enviroment = range(0, 8)
+static var yes_social = range(8, 14)
+static var yes_governance = range(14, 24)
+
+static var no_group = range(24, 48)
+static var no_enviroment = range(24, 32)
+static var no_social = range(32, 38)
+static var no_governance = range(38, 48)
+
+
+#static var enviroment_table = [
+	#"Combate ao desmatamento",
+	#"Preservação da biodiversidade",
+	#"Descarte adequado de dejetos e reciclagem",
+	#"Combate ao aquecimento global e redução da emissão de carbono",
+	#"Gestão de resíduos",
+	#"Diminuição da poluição",
+	#"Consumo consciente da água",
+	#"Uso de energias renováveis",
+#]
+#
+#static var social_table = [
+	#"Representatividade e inclusão de minorias",
+	#"Valorização dos funcionários",
+	#"Combate a questões como preconceitos, misoginia, assédio sexual...",
+	#"Cumprimento das leis trabalhistas e dos direitos humanos",
+	#"Combate a trabalhos infantis e análogos à escravidão",
+	#"Criação e desenvolvimento de projetos sociais",
+#]
+#
+#static var governance_table = [
+	#"Adoção de boas práticas administrativas",
+	#"Criação de canais de denúncia",
+	#"Independência do conselho administrativo",
+	#"Política de transparência",
+	#"Apresentação de relatórios financeiros",
+	#"Diversidade no conselho",
+	#"Responsabilidade fiscal",
+	#"Combate a corrupção",
+	#"Gestão de riscos",
+	#"Critérios transparentes sobre remuneração e planos de carreira"
+#]
+
+
+static var esg_half_table = [
+	"Combate ao desmatamento",
+	"Preservação da biodiversidade",
+	"Descarte adequado de dejetos e reciclagem",
+	"Combate ao aquecimento global e redução da emissão de carbono",
+	"Gestão de resíduos",
+	"Diminuição da poluição",
+	"Consumo consciente da água",
+	"Uso de energias renováveis",
+	"Representatividade e inclusão de minorias",
+	"Valorização dos funcionários",
+	"Combate a questões como preconceitos, misoginia, assédio sexual...",
+	"Cumprimento das leis trabalhistas e dos direitos humanos",
+	"Combate a trabalhos infantis e análogos à escravidão",
+	"Criação e desenvolvimento de projetos sociais",
+	"Adoção de boas práticas administrativas",
+	"Criação de canais de denúncia",
+	"Independência do conselho administrativo",
+	"Política de transparência",
+	"Apresentação de relatórios financeiros",
+	"Diversidade no conselho",
+	"Responsabilidade fiscal",
+	"Combate a corrupção",
+	"Gestão de riscos",
+	"Critérios transparentes sobre remuneração e planos de carreira"
+]
+
+
+func is_group_sum_larger_than(group: ESG_GROUPS, magnitude: int):
+	match group:
+		ESG_GROUPS.ENVIROMENT:
+			return group_sum(esg, yes_enviroment) > magnitude or group_sum(esg, no_enviroment)
+		ESG_GROUPS.SOCIAL:
+			return group_sum(esg, yes_social) > magnitude or group_sum(esg, no_social)
+		ESG_GROUPS.GOVERNANCE:
+			return group_sum(esg, yes_governance) > magnitude or group_sum(esg, no_governance)
+
+
+static func group_sum(location: Array, section: Array):
+	var sum = 0
+	for i in section:
+		sum += location[i]
+	return sum
+
+
+func self_group_sum(section: Array):
+	return group_sum(esg, section)
+
+
+func print_ESG(section, group_identification_str):
+	for i in section:
+		print(
+			str(i) +
+			" ESG: " +
+			esg_half_table[i % len(esg_half_table)] +
+			group_identification_str +
+			str(esg[i])
+		)
+
+
+func print_ESGs():
+	print_ESG(yes_enviroment, " --- Yes on Enviroment Score: ")
+	print_ESG(yes_social, " --- Yes on Social Score: ")
+	print_ESG(yes_governance, " --- Yes on Governance Score: ")
+	print_ESG(no_enviroment, " --- No on Enviroment Score: ")
+	print_ESG(no_social, " --- No on Social Score: ")
+	print_ESG(no_governance, " --- Yes on Governance Score: ")
--- scripts/score_manager.gd
+++ scripts/score_manager.gd
@@ -6,7 +6,6 @@ var money: int
 var revenue: int
 var skip: int
 
-
 static var DEFAULT_INITIAL_SCORE = 0 #Valor inicial de cada topico dentro do esg
 static var DEFAULT_INITIAL_MONEY = 1_000_000 #Dinheiro inicial
 static var DEFAULT_INITIAL_REVENUE = int(DEFAULT_INITIAL_MONEY / 10) #Salário inical
@@ -14,7 +13,7 @@ static var DEFAULT_WIN_CONDITION = 5 # "E" > 5 and "S" > 5 and "G" > 5
 static var DEFAULT_LOSE_CONDITION = -10 # "E" < -20 or "S" < -20 or "G" < -20
 static var DEFAULT_BIAS_MAGNITUDE = 5 #Filtro de quanto a soma de um grupo deve ter para ser elegivel como anti bad luck
 static var DEFAULT_BIAS_TRIGGER = int(DEFAULT_LOSE_CONDITION / 2) #Quão baixo o score do grupo deve estar para acionar a mecanica anti bad luck
-
+static var DEFAULT_INITIAL_SKIP = 3 #Quantidade de skips inicial 
 
 func _init():
 	score_initialize()
@@ -23,28 +22,10 @@ func _init():
 func score_initialize():
 	money = DEFAULT_INITIAL_MONEY
 	revenue = DEFAULT_INITIAL_REVENUE
+	skip = DEFAULT_INITIAL_SKIP
 	score = []
-	score.resize(len(Question.indexedNames))
+	score.resize(len(ESG.esg_half_table))
 	score.fill(DEFAULT_INITIAL_SCORE)
-
-
-func get_current_group_score(group: Array):
-	var sum = 0
-	for i in group:
-		sum += score[i]
-	return sum
-
-
-func get_current_enviroment_score():
-	return get_current_group_score(Question.yes_enviroment)
-
-
-func get_current_social_score():
-	return get_current_group_score(Question.yes_social)
-
-
-func get_current_governance_score():
-	return get_current_group_score(Question.yes_governance)
 
 
 func apply_revenue():
@@ -55,21 +36,23 @@ func apply_cost(cost):
 	money += cost 
 
 
-func print_score():
-	print("Money: " + str(money) + " Revenue: " + str(revenue))
-	print(" Enviroment Score: " + str(get_current_enviroment_score()) +
-		" Social Score: " + str(get_current_social_score()) +
-		" Governance Score: " + str(get_current_governance_score())
-	)
+func get_current_group_score(group: ESG.ESG_GROUPS):
+	match group:
+		ESG.ESG_GROUPS.ENVIROMENT:
+			return ESG.group_sum(score, ESG.yes_enviroment)
+		ESG.ESG_GROUPS.SOCIAL:
+			return ESG.group_sum(score, ESG.yes_social)
+		ESG.ESG_GROUPS.GOVERNANCE:
+			return ESG.group_sum(score, ESG.yes_governance)
 
 
 func check_bias_necessity():
-	if get_current_enviroment_score() < DEFAULT_BIAS_TRIGGER:
-		return Question.ESG_GROUPS.ENVIROMENT
-	if get_current_social_score() < DEFAULT_BIAS_TRIGGER:
-		return Question.ESG_GROUPS.SOCIAL
-	if get_current_governance_score() < DEFAULT_BIAS_TRIGGER:
-		return Question.ESG_GROUPS.GOVERNANCE
+	if get_current_group_score(ESG.ESG_GROUPS.ENVIROMENT) < DEFAULT_BIAS_TRIGGER:
+		return ESG.ESG_GROUPS.ENVIROMENT
+	if get_current_group_score(ESG.ESG_GROUPS.SOCIAL) < DEFAULT_BIAS_TRIGGER:
+		return ESG.ESG_GROUPS.SOCIAL
+	if get_current_group_score(ESG.ESG_GROUPS.GOVERNANCE) < DEFAULT_BIAS_TRIGGER:
+		return ESG.ESG_GROUPS.GOVERNANCE
 	return -1
 
 
@@ -81,14 +64,22 @@ func apply_scores(esg: Array, group: Array):
 
 
 func check_win_condition():
-	var env = get_current_enviroment_score() >= DEFAULT_WIN_CONDITION
-	var soc = get_current_social_score() >= DEFAULT_WIN_CONDITION
-	var gov = get_current_governance_score() >= DEFAULT_WIN_CONDITION
+	var env = get_current_group_score(ESG.ESG_GROUPS.ENVIROMENT) >= DEFAULT_WIN_CONDITION
+	var soc = get_current_group_score(ESG.ESG_GROUPS.SOCIAL) >= DEFAULT_WIN_CONDITION
+	var gov = get_current_group_score(ESG.ESG_GROUPS.GOVERNANCE) >= DEFAULT_WIN_CONDITION
 	return env and soc and gov
 
 
 func check_lose_condition():
-	var env = get_current_enviroment_score() <= DEFAULT_LOSE_CONDITION
-	var soc = get_current_social_score() <= DEFAULT_LOSE_CONDITION
-	var gov = get_current_governance_score() <= DEFAULT_LOSE_CONDITION
+	var env = get_current_group_score(ESG.ESG_GROUPS.ENVIROMENT) <= DEFAULT_LOSE_CONDITION
+	var soc = get_current_group_score(ESG.ESG_GROUPS.SOCIAL) <= DEFAULT_LOSE_CONDITION
+	var gov = get_current_group_score(ESG.ESG_GROUPS.GOVERNANCE) <= DEFAULT_LOSE_CONDITION
 	return env or soc or gov
+
+
+func print_score():
+	print("Money: " + str(money) + " Revenue: " + str(revenue))
+	print(" Enviroment Score: " + str(get_current_group_score(ESG.ESG_GROUPS.ENVIROMENT)) +
+		" Social Score: " + str(get_current_group_score(ESG.ESG_GROUPS.SOCIAL)) +
+		" Governance Score: " + str(get_current_group_score(ESG.ESG_GROUPS.GOVERNANCE))
+	)
