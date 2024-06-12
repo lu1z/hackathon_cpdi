6d3b7df27d21e9f96f9b38690492982e3f92948d
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-06-12 19:28:47 -0300

    outras alterações
    outras alterações

--- scripts/transitionScene.gd
+++ scripts/transitionScene.gd
@@ -8,8 +8,6 @@ func load_scene(target_scene,animation):
 	animatransition.play(animation)
 	await animatransition.animation_finished
 	get_tree().change_scene_to_file(target_scene)
-	animatransition.play_backwards(animation)
-	await animatransition.animation_finished
 	animatransition.play("RESET")
 	
 # Called when the node enters the scene tree for the first time.
