72c34eece58cecd9418c51f29af7b760c0537d56
Luiz Rosa <luiz.alberto.laus.rosa@gmail.com>
2024-06-14 21:26:46 -0300

    Character pictures and names updated from file

--- scripts/score_manager.gd
+++ scripts/score_manager.gd
@@ -9,10 +9,10 @@ var debt: int
 
 
 static var DEFAULT_INITIAL_SCORE = 0 #Valor inicial de cada topico dentro do esg
-static var DEFAULT_INITIAL_MONEY = 200_0 #Dinheiro inicial
+static var DEFAULT_INITIAL_MONEY = 50_000 #Dinheiro inicial
 static var DEFAULT_INITIAL_REVENUE = int(DEFAULT_INITIAL_MONEY / 20) #Salário inical
-static var DEFAULT_WIN_CONDITION = 50 # "E" > 5 and "S" > 5 and "G" > 5
-static var DEFAULT_LOSE_CONDITION = -50 # "E" < -20 or "S" < -20 or "G" < -20
+static var DEFAULT_WIN_CONDITION = 30 # "E" > 5 and "S" > 5 and "G" > 5
+static var DEFAULT_LOSE_CONDITION = -30 # "E" < -20 or "S" < -20 or "G" < -20
 static var DEFAULT_BIAS_MAGNITUDE = 5 #Filtro de quanto a soma de um grupo deve ter para ser elegivel como anti bad luck
 static var DEFAULT_BIAS_TRIGGER = int(DEFAULT_LOSE_CONDITION / 2) #Quão baixo o score do grupo deve estar para acionar a mecanica anti bad luck
 static var DEFAULT_INITIAL_SKIP = 3 #Quantidade de skips inicial
